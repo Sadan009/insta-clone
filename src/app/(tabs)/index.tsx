c9af7b5694fd1acd573b18b1fe9fbfae258d0611
Sadan009 <imamsadan009@gmail.com>
2025-01-05 19:47:50 +0530

    setup Nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,5 +1,9 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function FeedSreen() {
-  return <Text>Feed</Text>;
+  return (
+    <View className="bg-slate-400 flex-1 items-center justify-center">
+      <Text className="text-5xl font-bold text-white">Feed</Text>;
+    </View>
+  );
 }
